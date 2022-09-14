edcde8f859b42b13cef1c320746e503c1a134a51
WhoKnows <who@knows.eus>
2022-09-14 19:53:34 +0200

    Header mobile responsive

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,24 +1,59 @@
+import { selectionSetMatchesResult } from '@apollo/client/cache/inmemory/helpers';
+import { useState } from 'react';
 import { GoPrimitiveDot } from 'react-icons/go';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { EXPLORER_ROUTE, HOME_ROUTE, MARKET_ROUTE } from '~/constants';
 import './header.style.scss';
 
 const Header: React.FC = () => {
-  return (
-    <div id="bushido-header">
-      <div id="name-container">
+
+  const [ mobileMenu, setMobileMenu ] = useState(false);
+  const navigate = useNavigate();
+
+  const mobileClass = mobileMenu? 'active' : '';
+  const openMenu = mobileMenu ? 'open' : '';
+
+  const renderRoot = () => {
+    return (
         <Link to={HOME_ROUTE}>
           <span>bushido</span>
           <span id="dot"><GoPrimitiveDot/></span>
           <span>guide</span>
         </Link>
+    )
+  }
+
+  const navigateTo = (url: string) => {
+    setMobileMenu(false)
+    navigate(url)
+  }
+
+  return (
+    <div id="bushido-header">
+      <div id="name-container">
+        { renderRoot() }
       </div>
-      <div id="pages-container">
+      <div id="pages-container" className={`${mobileClass}`}>
         <nav>
-          <Link to={EXPLORER_ROUTE}>Explorer</Link>
-          <Link to={MARKET_ROUTE}>Market</Link>
+          {/*<Link to={EXPLORER_ROUTE}>Explorer</Link>
+          <Link to={MARKET_ROUTE}>Market</Link>*/}
+          <img src="/public/img/cover/bushido_stamp.png" onClick={() => navigateTo(HOME_ROUTE)}/>
+          <a onClick={() => navigateTo(EXPLORER_ROUTE)}>Explorer</a>
+          <a onClick={() => navigateTo(MARKET_ROUTE)}>Market</a>
         </nav>
+        <div id="mobile-name-container">
+          { renderRoot() }
+        </div>
+      </div>
+      <div className={`menu menu--close5 ${openMenu}`}  onClick={() => setMobileMenu(prevState => !prevState)}>
+        <div className="menu__icon">
+          <div className="menu__line menu__line--1"></div>
+          <div className="menu__line menu__line--2"></div>
+          <div className="menu__line menu__line--3"></div>
+          <div className="menu__line menu__line--4"></div>
+          <div className="menu__line menu__line--5"></div>
+        </div>
       </div>
     </div>
   )
